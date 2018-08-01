bbc75a2d6d0d62060e32be9eae8bbb3a7f752cb2
KlimekM <mklimek15@gmail.com>
2018-07-31 20:43:05 -0500

    Add front of card styled, extra properties on card object

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import logo from '../logo.svg';
 
 class Card extends Component {
   render() {
-    const { imgSrc, name, number } = this.props;
-
+    const { imgSrc, isFlipped, name, number } = this.props;
     return (
-      <div className="card" >
-        <p>{name}</p>
-        <img src={imgSrc} />
-        <p>{number}</p>
+      <div className="card">
+        {isFlipped && <div className="card-front">
+          <p className="card-front__text card-front__name">{name}</p>
+          <img className="card-front__image" src={imgSrc} />
+          <p className="card-front__text">{number}</p>
+        </div>}
+        {!isFlipped && <div className="card-back">
+          <img className="card-back__image" src={logo} />
+        </div>}
       </div>
     );
   }
--- src/utilities/cards.js
+++ src/utilities/cards.js
@@ -3,72 +3,96 @@ export const cards = [
     id: 1,
     name: 'Elflein',
     number: 65,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/ibydqfli9t97d02buhj6.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/ibydqfli9t97d02buhj6.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 2,
     name: 'Cousins',
     number: 8,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/khugu9xobhkodtmfpy5u.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/khugu9xobhkodtmfpy5u.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 3,
     name: 'Cook',
     number: 33,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/twzccqszxhlzugzn6hrg.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/twzccqszxhlzugzn6hrg.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 4,
     name: 'Diggs',
     number: 14,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/sxltpvpbpavogdipprhd.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/sxltpvpbpavogdipprhd.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 5,
     name: 'Thielen',
     number: 19,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/kcvahr3wkurwl5jffg9z.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/kcvahr3wkurwl5jffg9z.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 6,
     name: 'Rudolph',
     number: 82,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/kxt3v8kt6biiz4bo8ggb.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/kxt3v8kt6biiz4bo8ggb.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 7,
     name: 'Rhodes',
     number: 29,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/t7jgfjevqf035p7lohow.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/t7jgfjevqf035p7lohow.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 8,
     name: 'Smith',
     number: 22,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/hubfxex6nvkgc7hs2q2c.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/hubfxex6nvkgc7hs2q2c.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 9,
     name: 'Hunter',
     number: 99,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/kgmybmqktfvw2wpablhb.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/kgmybmqktfvw2wpablhb.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 10,
     name: 'Griffen',
     number: 97,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/snms7x5lmenlol8owgmt.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/snms7x5lmenlol8owgmt.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 11,
     name: 'Barr',
     number: 55,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/z4epxqhyydupw1syo3oo.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/z4epxqhyydupw1syo3oo.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
   {
     id: 12,
     name: 'Joseph',
     number: 98,
-    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/fii1ppk4wegn3cml8jfh.jpg'
+    imgSrc: 'https://res.cloudinary.com/nflclubs/image/private/t_thumb_squared/f_auto/vikings/fii1ppk4wegn3cml8jfh.jpg',
+    isFlipped: false,
+    isMatched: false,
   },
 ]
