fa9003306c338d1816e83ebc2500dac76e9d40e3
KlimekM <mklimek15@gmail.com>
2018-08-01 22:16:43 -0500

    Add markup and styles for winner modal

--- src/components/WinnerModal.js
+++ src/components/WinnerModal.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import logo from '../logo.svg';
 
 class WinnerModal extends Component {
   render() {
-    return null;
+    return (
+      <div className="modal-background">
+        <div className="modal">
+          <div className="modal__close">X</div>
+          <h1 className="modal__header">SKOL VIKES!</h1>
+          <img className="modal__image" src={logo} />
+          <p className="modal__text">You've matched them all!</p>
+          <div className="modal__reset-button">Reset Game</div>
+        </div>
+      </div>
+    )
   }
 }
 
